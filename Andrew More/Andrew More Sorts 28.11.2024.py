d960d09d5163ea1d0feeebdd7f0d838fb5abe18f
DrewsDoggo <79284572+DrewsDoggo@users.noreply.github.com>
2024-11-28 16:33:28 +0000

    Add files via upload

---
+++ Andrew More/Andrew More Sorts 28.11.2024.py
@@ -0,0 +1,92 @@
+def BubbleSort(items):
+   if len(items) <= 1:
+        return "There is nothing in the list!"
+   else:
+        for i in range(len(items) -1, 0, -1):
+            swap = False
+            for n in range(i):
+                if items[n] > items[n + 1]:
+                    items[n], items[n + 1] = items[n + 1], items[n]
+                    swap = True
+            if swap == False:
+                return f"The sorted list is: {items}"
+            else:
+                pass
+
+def InsertionSort(items):
+    n = len(items) 
+    if n <= 1:
+         print("There is only one item in list!")
+    else:
+        for i in range(1, n):
+            key = items[i]
+            j = i-1
+            while j >= 0 and key < items[j]:
+                items[j+1] = items[j]
+                j -= 1
+            items[j+1] = key
+    print(f"Sorted lits is {items}") 
+
+
+def MergeSort(items):
+    if len(items) > 1:
+        mid = len(items) // 2
+        left_half = items[:mid:]
+        right_half = items[mid:]
+        MergeSort(left_half)
+        MergeSort(right_half)
+        i = j = k = 0
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                items[k] = left_half[i]
+                i += 1
+            else:
+                items[k] = right_half[j]
+            k += 1
+        while i < len(left_half):
+            items[k] = left_half[i]
+            i += 1
+            k += 1
+        while i < len(right_half):
+            items[k] = right_half[j]
+            j += 1
+            k += 1
+    print(items)    
+
+def SelectionSort(array, size):
+    for ind in range(size):
+        min_index = ind
+        for j in range(ind + 1, size):
+            if array[j] < array[min_index]:
+                min_index = j
+        (array[ind], array[min_index]) = (array[min_index], array[ind])
+
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    else:
+        pivot = arr[0]
+        left = [x for x in arr[1:] if x < pivot]
+        right = [x for x in arr[1:] if x >= pivot]
+        return quicksort(left) + [pivot] + quicksort(right)
+
+items = []
+print("Enter anything but a number to begin the sort!")
+while True:
+    try:
+        toadd = float(input("Enter number to put in list: "))
+        items = items + [toadd]
+    except ValueError:
+        # print(BubbleSort(items))
+        # InsertionSort(items)
+        # print(f"The sorted list is: {items}")
+        MergeSort(items)
+        # print("Sorted array is")
+        # for i in range(n):
+        #     print("%d" % items[i],end=" ")
+        # size = len(items)
+        # SelectionSort(items,size)
+        # print(f"Sorted array is {items}")
+        # sortarr = quicksort(items)
+        # print(f"Sorted list is: {sortarr}")
+        exit()
